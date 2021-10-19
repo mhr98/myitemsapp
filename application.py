e4f75046695cb37e63bab9dd29315282ee4d5112
mohammad <mohamm.ad@outlook.com>
2021-10-20 01:42:25 +0300

    adding coment

--- application.py
+++ application.py
@@ -7,6 +7,8 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+"""after coment"""
  
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
